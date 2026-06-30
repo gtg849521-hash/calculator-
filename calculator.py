874dde8c48a4807aee291f52370a974e9c7b9555
gtg849521-hash <gtg849521@gmail.com>
2026-06-30 16:33:37 +0500

    done

--- calculator.py
+++ calculator.py
@@ -1,4 +1,49 @@
-a = int(input("enter a number "))
-b = int(input("enter a number "))
-sum = a + b
-print(sum )
+
+running = True 
+while running:
+    cal = input("what would u like to do?" \
+"add,sub,per,dvd,mul:")
+    if (cal == "add"):
+        a = int(input("enter the first number "))
+        b = int(input("enter the second number "))
+        sum = a + b
+        print(sum)
+    elif (cal == "sub"):
+          a = int(input("enter the first number "))
+          b = int(input("enter the second number"))
+          sub = a - b
+          print(sub)
+    elif (cal == "per"):
+          a = int(input("enter the first number "))
+          b = int(input("enter the second(out of) number"))
+          per = (a / b )*100
+          print(per)
+    elif (cal == "dvd"):
+         a = int(input("enter the first number "))
+         b = int(input("enter the second number"))
+         if (b == 0):
+              print("you cant divide 0")
+         else:
+          dvd = a / b
+          print(dvd)
+    elif (cal == "mul"):
+          a = int(input("enter the first number "))
+          b = int(input("enter the second number"))
+          mul = a*b
+          print(mul)
+    else:
+         print("invalid ")
+    run = input("would you like to calculate again?,yes or no")
+    if (run== "no"):
+         running = False 
+         print("thanks for using calculator!")
+
+
+ 
+
+
+
+
+
+
+
